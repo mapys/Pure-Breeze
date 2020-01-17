e3d312e0ca1c15d12ef0379149b99ccd99469a53
Mark Allan Paul Sebastian <mapys@cloudstaff.com>
2020-01-17 12:59:09 +0800

    Lesson 85

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
-  appName = 'Recipe Book'
+  @Output() featureSelected = new EventEmitter<string>();
+  appName = 'Recipe Book';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(feature: string){
+    this.featureSelected.emit(feature);
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe = [
+  recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a test', 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg'),
     new Recipe('A Test Recipe', 'This is a test', 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg')
   ];
